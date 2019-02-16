7be2e7a21149a22f3e780e741be6ef04682b2e8d
Damian Matyjaszek <tougepl@gmail.com>
2019-02-16 18:16:07 +0100

    Add pug support

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,9 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('pug', {
+  test: /\.pug$/,
+  loader: 'pug-plain-loader'
+})
+
 module.exports = environment
